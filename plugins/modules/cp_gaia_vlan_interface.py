81f03453f72a45b1f9d4fcfd05282a883f806221
nitzanefdome9 <nitzanef@checkpoint.com>
2020-10-22 11:26:52 +0300

    vlan interface - show and set

---
+++ plugins/modules/cp_gaia_vlan_interface.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Ansible module to manage CheckPoint Firewall (c) 2019
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from __future__ import (absolute_import, division, print_function)
+
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = """
+module: cp_gaia_physical_interface
+author: Yuval Feiger (@chkp-yuvalfe)
+description:
+- Set Physical interface
+short_description: Set Physical interface
+version_added: '2.9'
+options:
+  auto_negotiation:
+    description: Activating Auto-Negotiation will skip the speed and duplex configuration
+    required: false
+    type: bool
+  comments:
+    description: interface Comments.
+    required: false
+    type: str
+  duplex:
+    description: Duplex for the interface. Duplex is not relevant when 'auto-negotiation' is enabled
+    required: false
+    type: str
+  enabled:
+    description: interface State.
+    required: false
+    type: bool
+  ipv4_address:
+    description: Interface IPv4 address.
+    required: false
+    type: str
+  ipv4_mask_length:
+    description: Interface IPv4 address mask length.
+    required: false
+    type: int
+  ipv6_address:
+    description: Interface IPv6 address.
+    required: false
+    type: str
+  ipv6_autoconfig:
+    description: Configure IPv6 auto-configuration.
+    required: false
+    type: bool
+  ipv6_mask_length:
+    description: Interface IPv6 address mask length.
+    required: false
+    type: int
+  mac_addr:
+    description: Configure hardware address.
+    required: false
+    type: str
+  monitor_mode:
+    description: set monitor mode for the interface off/on.
+    required: false
+    type: bool
+  mtu:
+    description: interface mtu.
+    required: false
+    type: int
+  name:
+    description: interface name.
+    required: true
+    type: str
+  rx_ringsize:
+    description: Set receive buffer size for interface.
+    required: false
+    type: int
+  speed:
+    description: Interface link speed. Speed is not relevant when 'auto-negotiation' is enabled
+    required: false
+    type: str
+  tx_ringsize:
+    description: Set transmit buffer size for interfaces.
+    required: false
+    type: int
+
+"""
+
+EXAMPLES = """
+- name: Set comment field of a physical interface
+  cp_gaia_physical_interface:
+    comments: eth0 interface
+    enabled: true
+    name: eth0
+
+"""
+
+RETURN = """
+physical_interface:
+  description: The updated interface details.
+  returned: always.
+  type: dict
+"""
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.check_point.gaia.plugins.module_utils.checkpoint import set_api_call
+
+
+def main():
+    # arguments for the module:
+    fields = dict(
+        ipv6_address=dict(required=False, type="str"),
+        ipv4_mask_length=dict(required=False, type="int"),
+        name=dict(required=True, type="str"),
+        ipv6_autoconfig=dict(required=False, type="bool"),
+        enabled=dict(required=False, type="bool"),
+        comments=dict(required=False, type="str"),
+        mtu=dict(required=False, type="int"),
+        ipv4_address=dict(required=False, type="str"),
+        ipv6_mask_length=dict(required=False, type="int")
+    )
+    module = AnsibleModule(argument_spec=fields, supports_check_mode=True)
+    api_call_object = 'vlan-interface'
+    ignore = ["status"]
+    keys = ["name"]
+
+    res = set_api_call(module, api_call_object, ignore, keys)
+    module.exit_json(**res)
+
+
+if __name__ == "__main__":
+    main()
